278d4e002d7d6a3243ac691e020abd6906fa97d6
Mikhail Korobov <kmike84@gmail.com>
2012-07-21 15:37:07 +0600

    remove useless test

--- tests/test_alpha_map.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-import datrie
-
-def test_alphamap():
-    # we can't check anything; make sure it at least doesn't crash
-    alpha_map = datrie.AlphaMap()
-    alpha_map.add_range('a', 'z')
-    del alpha_map
