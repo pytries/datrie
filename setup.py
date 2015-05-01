fefa20c757e6489eedecbc2134c43aa2f75b94e8
Sergei Lebedev <superbobry@gmail.com>
2015-05-01 13:18:09 +0300

    Cleaned up 'setup.py'

--- setup.py
+++ setup.py
@@ -1,52 +1,58 @@
 #! /usr/bin/env python
+"""Super-fast, efficiently stored Trie for Python."""
+
 import os
+
 from distutils.core import setup
 from distutils.extension import Extension
-#from Cython.Distutils import build_ext
 
 LIBDATRIE_DIR = 'libdatrie/datrie'
 LIBDATRIE_FILE_NAMES = [
     'alpha-map.c', 'darray.c', 'fileutils.c', 'tail.c', 'trie.c',
     'dstring.c', 'trie-string.c',
 ]
-LIBDATRIE_FILES = [os.path.join(LIBDATRIE_DIR, name) for name in LIBDATRIE_FILE_NAMES]
+LIBDATRIE_FILES = [os.path.join(LIBDATRIE_DIR, name)
+                   for name in LIBDATRIE_FILE_NAMES]
+
+DESCRIPTION = __doc__
+LONG_DESCRIPTION = open('README.rst').read() + open('CHANGES.rst').read()
+LICENSE = 'LGPLv2+'
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+    'Programming Language :: Cython',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Text Processing :: Linguistic'
+]
 
 setup(
     name="datrie",
     version="0.7",
-    description="Super-fast, efficiently stored Trie for Python",
-    long_description = open('README.rst').read() + open('CHANGES.rst').read(),
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
-    url='https://github.com/kmike/datrie/',
-    #packages = ['datrie'],
-    #cmdclass = {'build_ext': build_ext},
-
-    ext_modules = [
-        Extension(
-            "datrie",
-            ['src/datrie.c', 'src/cdatrie.c', 'src/stdio_ext.c'] + LIBDATRIE_FILES,
-            include_dirs=['libdatrie'],
-        )
+    license=LICENSE,
+    url='https://github.com/kmike/datrie',
+    classifiers=CLASSIFIERS,
+    ext_modules=[
+        Extension("datrie", [
+            'src/datrie.c',
+            'src/cdatrie.c',
+            'src/stdio_ext.c'
+        ] + LIBDATRIE_FILES, include_dirs=['libdatrie'])
     ],
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-        'Programming Language :: Cython',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Text Processing :: Linguistic',
-    ],
-
+    tests_requires=["pytest", "hypothesis"]
 )
