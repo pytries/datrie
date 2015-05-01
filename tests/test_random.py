90d42d8d2f5e7a7f15cd13c04b1d97522c9b6acc
Sergei Lebedev <superbobry@gmail.com>
2015-05-01 19:22:32 +0300

    Disabled randomized tests for Python2.6

--- tests/test_random.py
+++ tests/test_random.py
@@ -4,42 +4,44 @@ from __future__ import absolute_import, unicode_literals
 
 import pickle
 import string
+import sys
 
 import datrie
-from hypothesis import given
-from hypothesis.specifiers import strings
+
+# This is ugly, but hypothesis only supports Python2.7+ at the moment.
+if sys.version_info[:2] >= (2, 7):
+    from hypothesis import given
+    from hypothesis.specifiers import strings
+
+    printable_strings = [strings(string.printable)]
+
+    @given(printable_strings)
+    def test_contains(words):
+        trie = datrie.Trie(string.printable)
+        for i, word in enumerate(set(words)):
+            trie[word] = i
+
+        for i, word in enumerate(set(words)):
+            assert word in trie
+            assert trie[word] == i
 
 
-printable_strings = [strings(string.printable)]
+    @given(printable_strings)
+    def test_len(words):
+        trie = datrie.Trie(string.printable)
+        for i, word in enumerate(set(words)):
+            trie[word] = i
+
+        assert len(trie) == len(set(words))
 
 
-@given(printable_strings)
-def test_contains(words):
-    trie = datrie.Trie(string.printable)
-    for i, word in enumerate(set(words)):
-        trie[word] = i
+    @given(printable_strings)
+    def test_pickle_unpickle(words):
+        trie = datrie.Trie(string.printable)
+        for i, word in enumerate(set(words)):
+            trie[word] = i
 
-    for i, word in enumerate(set(words)):
-        assert word in trie
-        assert trie[word] == i
-
-
-@given(printable_strings)
-def test_len(words):
-    trie = datrie.Trie(string.printable)
-    for i, word in enumerate(set(words)):
-        trie[word] = i
-
-    assert len(trie) == len(set(words))
-
-
-@given(printable_strings)
-def test_pickle_unpickle(words):
-    trie = datrie.Trie(string.printable)
-    for i, word in enumerate(set(words)):
-        trie[word] = i
-
-    trie = pickle.loads(pickle.dumps(trie))
-    for i, word in enumerate(set(words)):
-        assert word in trie
-        assert trie[word] == i
+        trie = pickle.loads(pickle.dumps(trie))
+        for i, word in enumerate(set(words)):
+            assert word in trie
+            assert trie[word] == i
