2c209c892aa1cfa89dff7a84b6ddd10f67c9af2f
Sergei Lebedev <superbobry@gmail.com>
2016-03-12 00:23:38 +0300

    Test and build on Python3.5
      I've also fetched the latest PowerShell magic from the wonderful
      ogrisel/python-appveyor-demo project

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Scientific/Engineering :: Information Analysis',
