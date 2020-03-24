4c98b77f7f22bfbf16c840bfcc0eae1fa4ca1119
Thomas A Caswell <tcaswell@gmail.com>
2020-03-24 18:27:05 -0400

    MNT: update trove classifier

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Scientific/Engineering :: Information Analysis',
