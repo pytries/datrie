c67d07cb7cf6c05e20e9e750452ffd35b1a2cfbf
KOLANICH <kolan_n@mail.ru>
2021-01-19 14:37:48 +0300

    pytest-runner is not needed for building the package

--- setup.py
+++ setup.py
@@ -60,5 +60,5 @@ setup(name="datrie",
           "include_dirs": [LIBDATRIE_DIR]})],
       ext_modules=ext_modules,
       python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-      setup_requires=["pytest-runner", 'Cython>=0.28'],
+      setup_requires=['Cython>=0.28'],
       tests_require=["pytest", "hypothesis"])
